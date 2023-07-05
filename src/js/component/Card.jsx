de2c0071c553541dd85141cbe8bd61fb8f167ee8
eunice_sin_h <71563392+EuniceHG@users.noreply.github.com>
2023-07-05 07:15:16 +0000

    AddingExtraStyles

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 
 const Card = (props) => {
     return (
-        <div className="card">
+        <div className="card border rounded">
             <img src={props.image} className="card-img-top" alt="..."></img>
             <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.description}</p>
+                <h5 className="card-title text-center pt-3">{props.title}</h5>
+                <p className="card-text text-center px-3">{props.description}</p>
                 <div className="card-footer">
                     <a href={props.buttonURL} target="_blank" className="btn btn-primary">{props.buttonLabel}</a>
                 </div>
